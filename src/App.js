4b435a1da221c8bd25fcd27467e1680ee41d4f0f
vikaskumar0205 <vikaskumar808425@gmail.com>
2023-07-19 09:20:06 +0530

    adding summary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Summary from './Components/Summary/Summary';
 
 const App = ()=> {
   return (<div>
     <Header/>
+    <Summary/>
   </div>);
 }
 
---
+++ src/Components/Summary/Summary.jsx
@@ -0,0 +1,19 @@
+import "./Summary.css";
+
+const Summary = () => {
+  return (
+    <div className="summary">
+      <h1>Delicious Food, Delivered To You</h1>
+      <p className="summary_paragraph_1">
+        Choose your favourite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+      <p className="summary_paragraph_2">
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </div>
+  );
+};
+
+export default Summary;
