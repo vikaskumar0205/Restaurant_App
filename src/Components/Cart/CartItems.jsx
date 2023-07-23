6d417afbce43edcb298297997254520c62ceb69d
vikaskumar0205 <vikaskumar808425@gmail.com>
2023-07-23 15:55:56 +0530

    added increase and decrease button

--- src/Components/Cart/CartItems.jsx
+++ src/Components/Cart/CartItems.jsx
@@ -5,53 +5,58 @@ import CartContext from "../../Store/CartContex";
 const CartItems = (props) => {
   let cartCtx = useContext(CartContext);
 
-  const increaseItemHanler=(e)=> {
-    // console.log(e.target.parentElement.parentElement);
-    props.onIncreaseItem(e.target.parentElement.parentElement.id);
-  }
+  const increaseItemHanler = (id) => {
+    props.onIncreaseItem(id);
+  };
 
-  const removeItemHandler=(e)=> {
-    // console.log(e.target.parentElement);
-    props.onRemoveItem(e.target.parentElement.parentElement.id);
-  }
+  const removeItemHandler = (id) => {
+    props.onRemoveItem(id);
+  };
 
-  const deleteItemHandler=(e)=> {
-    props.onDeleteItem(e.target.parentElement.parentElement.id);
-  }
+  const deleteItemHandler = (id) => {
+    props.onDeleteItem(id);
+  };
 
-  if(cartCtx.items.length===0) {
-    return <h3 className={classes.empty}>No items found, Please add items</h3>
+  if (cartCtx.items.length === 0) {
+    return <h3 className={classes.empty}>No items found, Please add items</h3>;
   }
 
   return (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((cartItem) => (
         <li key={cartItem.id} id={cartItem.id} className={classes["cart-item"]}>
-          <div>{cartItem.name}</div>
-          <div className={classes.amount}>${cartItem.price}</div>
-          <div className={classes["changed-button"]}>
-            <button
-              type="button"
-              className={classes.decrese}
-              style={{ fontSize: "20px" }}
-              onClick={removeItemHandler}
-            >
-              -
-            </button>
-            <h4>x{cartItem.amount}</h4>
-            <button
-              type="button"
-              className={classes.increase}
-              style={{ fontSize: "20px" }}
-              onClick={increaseItemHanler}
-            >
-              +
-            </button>
-          </div>
-          <div className={classes["delete-button"]}>
-            <button type="button" className={classes.delete} onClick={deleteItemHandler}>
-              Delete
-            </button>
+          <div className={classes.name}>{cartItem.name}</div>
+          <div className={classes.about}>
+            <div className={classes.amount}>${cartItem.price}</div>
+            <div className={classes["changed-button"]}>
+              <button
+                type="button"
+                className={classes.decrese}
+                style={{ fontSize: "20px" }}
+                onClick={removeItemHandler.bind(null, cartItem.id)}
+              >
+                -
+              </button>
+              <h4>x{cartItem.amount}</h4>
+              <button
+                type="button"
+                className={classes.increase}
+                style={{ fontSize: "20px" }}
+                onClick={increaseItemHanler.bind(null, cartItem.id)}
+              >
+                +
+              </button>
+            </div>
+
+            <div className={classes["delete-button"]}>
+              <button
+                type="button"
+                className={classes.delete}
+                onClick={deleteItemHandler.bind(null, cartItem.id)}
+              >
+                Delete
+              </button>
+            </div>
           </div>
         </li>
       ))}
